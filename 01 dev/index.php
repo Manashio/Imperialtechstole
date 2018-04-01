a48563b029b49b62540774f2d3f49298e7061e1e
Manash97 <manashbharali79@gmail.com>
2018-04-01 12:52:47 +0530

    paragraph changed

--- 01 dev/index.php
+++ 01 dev/index.php
@@ -58,12 +58,7 @@
 			<div class="col-md-6">
 				<h4 class="title-heading pt-5">Need help?</h4>
 				<p class="pt-5 pb-5">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				hello world
 				</p>
 			</div>
 
