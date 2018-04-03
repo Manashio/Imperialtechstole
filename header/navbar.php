702afc0eedd95dcb1a975062e84bf0191e0c06a7
Manash97 <manashbharali79@gmail.com>
2018-04-04 01:09:28 +0530

    clean nav bar

--- header/navbar.php
+++ header/navbar.php
@@ -1,33 +1,5 @@
 <div class="menu-top">
           <div class="row">
-
-               <!-- <div class="col-md-3">
-                    <div class="">
-                         <a href="index.php" class="brand-icon text-center" >Brand</a>
-                    </div>    
-			   </div>
-			   
-               <div class="col-md-5">
-               		<div class="search1">
-                         <form action="">
-                    	     <i class="fas fa-search searchicon"></i> 
-                        	  <input class="search1 ml-2" type="text" placeholder="Search here"> 
-                         </form>
-                    </div>
-               </div>
-
-               <div class="col-md-1">
-                    <a href="#" class="btn org" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Get started</a>
-               </div>
-
-               <div class="col-md-3">
-                    <div id="target" class="hum-btn pt-3 pr-5">   
-                         <span></span>
-                         <span></span>
-                         <span></span>
-                    </div>
-			   </div> -->
-			   
 			   <div class="col-3">
 			   		<a href="index.php" class="brand-icon text-center" >Brand</a>
 				</div>
@@ -36,24 +8,19 @@
                          <form action="">
                     	     <i class="fas fa-search searchicon"></i> 
 							  <input class="search1 ml-2" type="text" placeholder="Search here"> 
-							 
                          </form>
 					</div>
 				</div>
-
 				<div class="col">
-				<a href="#" class="btn org" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Get started</a>
+					<a href="#" class="btn org" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Get started</a>
 					<div id="target" class="hum-btn pt-3 pr-5">   
                          <span></span>
                          <span></span>
                          <span></span>
                     </div>
 				</div>
-
           </div>
-          
      </div>
-     
 	<nav id="menu">
 			<ul class="sidenav">
 				<li><a href="index.php"><i class="fas fa-home mr-2"></i>  Home</a></li>
