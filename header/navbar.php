5c4456f0cd74969193e24dc40465200747829dbc
nubul <nubul.machary@triontechnologies.com>
2018-04-03 22:27:03 +0530

    model response made on all pages

--- header/navbar.php
+++ header/navbar.php
@@ -43,3 +43,46 @@
      <div class="div" id="block"></div>
 
 
+<!--Model-->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document" >
+    <div class="modal-content" style="width:100%!important;">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Ask me anything</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="store/mail.php" method="POST">
+          <div class="form-group">
+            <label for="name" class="col-form-label">Your name:</label>
+            	<input type="text" class="form-control" id="name" name="name" placeholder="John Doe">
+          </div>
+
+		<div class="form-group">
+            <label for="email" class="col-form-label">Your Email:</label>
+            	<input type="text" class="form-control" id="email" name="email" placeholder="Johndoe@gmail.com">
+          </div>
+
+		<div class="form-group">
+            <label for="phone" class="col-form-label">Your Phone no:</label>
+            	<input type="text" class="form-control" id="phone" name="phone" placeholder="9706300000">
+          </div>
+
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Message:</label>
+            <textarea class="form-control" id="message-text" name="message"></textarea>
+          </div>
+					<div class="modal-footer">
+						<!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
+						<button type="submit" class="btn btn-primary">Send message</button>
+					</div>
+        </form>
+
+
+      </div>
+     </div>
+  </div>
+</div>
+
