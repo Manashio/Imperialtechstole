da66696e18a4f05a446416b5e8484eee437bb377
Manash97 <manashbharali79@gmail.com>
2018-04-16 17:05:27 +0530

    admin : added + new table users added for admin

--- sql/imperial_admin (1).sql
+++ sql/imperial_admin (1).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 16, 2018 at 10:51 AM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 5.6.33
+-- Generation Time: Apr 16, 2018 at 12:56 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -1358,6 +1358,26 @@ INSERT INTO `tbl_slider` (`fld_id`, `fld_title`, `fld_content`, `fld_image`, `fl
 (14, 'slider9', 'slider9', 'AERO92221505954385.jpg', '', '', 'Active'),
 (15, 'slider10', 'slider10', 'AERO44061505954404.jpg', '', '', 'Active');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `time_stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `time_stamp`) VALUES
+(1, 'admin', 'password', '2018-04-16 10:51:34');
+
 --
 -- Indexes for dumped tables
 --
@@ -1564,6 +1584,12 @@ ALTER TABLE `tbl_slider`
   ADD PRIMARY KEY (`fld_id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -1764,6 +1790,12 @@ ALTER TABLE `tbl_service`
 --
 ALTER TABLE `tbl_slider`
   MODIFY `fld_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
